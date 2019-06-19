f36cec0f6bbb185ccc84e132708f5d29ea29f35c
polinakhari <polina14.05@yandex.ru>
2019-06-19 13:58:15 +0300

    Update index.js

--- BASKET/index.js
+++ BASKET/index.js
@@ -1,84 +1,95 @@
 'use strict';
 
-const budget = 450,
-      goods = {},
-      budgetValueElement = document.getElementsByClassName('budget__budget-value')[0],
-      goodElements = document.getElementsByClassName('good'),
-      basketElement = document.getElementsByClassName('basket')[0],
-      basketContainerElement = document.getElementsByClassName('basket__goods-container')[0],
-      progressBar = document.getElementsByClassName('basket__progress-bar')[0];
-      progressBar.style.width = 0;
+const budget = 50000,
+  goods = {},
+  budgetValueElement = document.getElementsByClassName('budget__budget-value')[0],
+  goodElements = document.getElementsByClassName('good'),
+  basketElement = document.getElementsByClassName('basket')[0],
+  basketContainerElement = document.getElementsByClassName('basket__goods-container')[0],
+  progressBar = document.getElementsByClassName('basket__progress-bar')[0];
+
+progressBar.style.width = 0;
 let purchaseAmount = 0;
 
 function colorProgressBar(fullness) {
-    if (fullness <= 50) {
-        progressBar.classList.add('bg-success');
-        progressBar.classList.remove('bg-warning');
-        progressBar.classList.remove('bg-danger');
-    } else if (fullness > 50 && fullness <= 85) {
-        progressBar.classList.add('bg-warning');
-        progressBar.classList.remove('bg-danger');
-        progressBar.classList.remove('bg-success');
-    } else {
-        progressBar.classList.add('bg-danger');
-        progressBar.classList.remove('bg-warning');
-        progressBar.classList.remove('bg-success');
-    }
+  if (fullness <= 50) {
+    progressBar.classList.add('bg-success');
+    progressBar.classList.remove('bg-warning');
+    progressBar.classList.remove('bg-danger');
+  } else if (fullness > 50 && fullness <= 85) {
+    progressBar.classList.add('bg-warning');
+    progressBar.classList.remove('bg-danger');
+    progressBar.classList.remove('bg-success');
+  } else {
+    progressBar.classList.add('bg-danger');
+    progressBar.classList.remove('bg-warning');
+    progressBar.classList.remove('bg-success');
+  }
 }
 
 budgetValueElement.textContent = budget;
 
 for (const goodElement of goodElements) {
-    goodElement.addEventListener('dragstart', (event) => {
-        event.dataTransfer.setData(
-            'application/json',
-            JSON.stringify([event.target.children[3].dataset.price, event.target.id]),
-        );
-    });
+  goodElement.addEventListener('dragstart', (event) => {
+    event.dataTransfer.setData(
+      'application/json',
+      JSON.stringify([event.target.children[3].dataset.price, event.target.id]),
+    );
+  });
 }
 
 basketElement.addEventListener('drop', (event) => {
-    const data = event.dataTransfer.getData('application/json');
+  const data = event.dataTransfer.getData('application/json');
 
-    if (data) {
-        const [price, goodId] = JSON.parse(data);
+  if (data) {
+    const [price, goodId] = JSON.parse(data);
 
-        basketElement.dispatchEvent(new CustomEvent('order', { detail: { price, goodId } }));
-    }
+    basketElement.dispatchEvent(new CustomEvent('order', {
+      detail: {
+        price,
+        goodId
+      }
+    }));
+  }
 });
 
 basketElement.addEventListener('dragover', event => event.preventDefault());
 
-basketElement.addEventListener('order', ({ detail: { price: stringPrice, goodId } }) => {
-    const price = +stringPrice;
-    console.log(price);
+basketElement.addEventListener('order', ({
+  detail: {
+    price: stringPrice,
+    goodId
+  }
+}) => {
+  const price = +stringPrice;
+  console.log(price);
 
-    if (purchaseAmount + price > budget) {
-        alert('Вы исчерпали свой бюджет!');
+  if (purchaseAmount + price > budget) {
+    alert('Бюджет исчерпан');
+  } else {
+    const orderedGoodElement = document.getElementById(goodId).cloneNode(true);
+    let count = 1;
+
+    if (goods[orderedGoodElement.id]) {
+      count = ++goods[orderedGoodElement.id];
+      const countElement = [...basketContainerElement.children].filter(goodElement => goodElement.children[0].dataset.id === `${goodId}-count`)[0];
+      countElement.children[0].textContent = count;
     } else {
-        const orderedGoodElement = document.getElementById(goodId).cloneNode(true);
-        let count = 1;
-
-        if (goods[orderedGoodElement.id]) {
-            count = ++goods[orderedGoodElement.id];
-            const countElement = [...basketContainerElement.children].filter(goodElement => goodElement.children[0].dataset.id === `${goodId}-count`)[0];
-            countElement.children[0].textContent = count;
-        } else {
-            goods[orderedGoodElement.id] = count;
-            const counterElement = orderedGoodElement.children[0];
-            document.getElementById('info').classList.add("hidden");
-            counterElement.dataset.id = counterElement.id;
-            counterElement.removeAttribute('id');
-            orderedGoodElement.removeAttribute('id');
-            orderedGoodElement.setAttribute('draggable', false);
-            counterElement.textContent = count;
-            basketContainerElement.appendChild(orderedGoodElement);
-        }
-
-        purchaseAmount += price;
-        const fullness = (purchaseAmount / budget) * 100;
-        progressBar.style.width = `${fullness}%`;
-        progressBar.innerHTML = `&euro;${purchaseAmount}/` + budget;
-        colorProgressBar(fullness);
+      goods[orderedGoodElement.id] = count;
+      const counterElement = orderedGoodElement.children[0];
+      document.getElementById('info').classList.add("hidden");
+      counterElement.dataset.id = counterElement.id;
+      counterElement.removeAttribute('id');
+      orderedGoodElement.removeAttribute('id');
+      orderedGoodElement.setAttribute('draggable', false);
+      counterElement.textContent = count;
+      basketContainerElement.appendChild(orderedGoodElement);
     }
+
+    purchaseAmount += price;
+    const fullness = (purchaseAmount / budget) * 100;
+    progressBar.style.width = `${fullness}%`;
+    document.getElementsByClassName('basket__progress-title')[0].innerHTML = "Итого: " + `${purchaseAmount}` + ' &#x20BD; ' + " Остаток: " + `${budget - purchaseAmount}` + ' &#x20BD;';
+    colorProgressBar(fullness);
+  }
 });
